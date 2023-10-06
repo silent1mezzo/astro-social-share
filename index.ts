924d651ce64ed40e59f267172d62933770a95c4c
Adam McKerlie <adammckerlie@gmail.com>
2023-10-05 23:26:46 -0400

    Add Email Sharing, re-order imports

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-export { default as TwitterShareButton } from './src/Twitter.astro';
+export { default as EmailShareButton } from './src/Email.astro';
 export { default as FacebookShareButton } from './src/Facebook.astro';
 export { default as HackerNewsShareButton } from './src/HackerNews.astro';
 export { default as RedditShareButton } from './src/Reddit.astro';
+export { default as TwitterShareButton } from './src/Twitter.astro';
 export { default as SocialShare } from './src/SocialShare.astro';
